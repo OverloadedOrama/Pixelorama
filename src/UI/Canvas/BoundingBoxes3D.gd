cfa9be741f046a6fb607f6b63d3460106765b077
Emmanouil Papadeas <manoschool@yahoo.gr>
2022-12-31 04:34:01 +0200

    Draw gizmo arrows and circles
    Not interactable yet

--- src/UI/Canvas/BoundingBoxes3D.gd
+++ src/UI/Canvas/BoundingBoxes3D.gd
@@ -4,7 +4,7 @@ var points_per_object := {}
 var selected_color := Color.white
 var hovered_color := Color.gray
 
-#var gizmo_used := false
+var gizmos_origin: Vector2
 var proj_right_local: Vector2
 var proj_up_local: Vector2
 var proj_back_local: Vector2
@@ -24,8 +24,7 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if not event is InputEventMouseButton:
 		return
-	var pos: Vector2 = Global.canvas.current_pixel - gizmos.rect_position
-#	print(pos, " ", gizmo_rotation_z)
+	var pos: Vector2 = Global.canvas.current_pixel - gizmos_origin
 	if event.button_index == BUTTON_LEFT:
 		if event.pressed:
 			if Geometry.is_point_in_polygon(pos, gizmo_rotation_x):
@@ -57,22 +56,21 @@ func get_points(camera: Camera, object3d: Object3D) -> void:
 	points_per_object[object3d] = points
 	if object3d.selected:
 		gizmos.visible = true
-		gizmos.rect_position = camera.unproject_position(object3d.translation)
+		gizmos_origin = camera.unproject_position(object3d.translation)
 
-		var right :Vector3= object3d.translation + object3d.transform.basis.x
-		var up :Vector3= object3d.translation + object3d.transform.basis.y
-		var back :Vector3= object3d.translation + object3d.transform.basis.z
+		var right: Vector3 = object3d.translation + object3d.transform.basis.x
+		var up: Vector3 = object3d.translation + object3d.transform.basis.y
+		var back: Vector3 = object3d.translation + object3d.transform.basis.z
 
-		var proj_right :Vector2= object3d.camera.unproject_position(right)
-		var proj_up :Vector2= object3d.camera.unproject_position(up)
-		var proj_back :Vector2= object3d.camera.unproject_position(back)
+		var proj_right: Vector2 = object3d.camera.unproject_position(right)
+		var proj_up: Vector2 = object3d.camera.unproject_position(up)
+		var proj_back: Vector2 = object3d.camera.unproject_position(back)
 
-		proj_right_local = proj_right - gizmos.rect_position
-		proj_up_local = proj_up - gizmos.rect_position
-		proj_back_local = proj_back - gizmos.rect_position
+		proj_right_local = proj_right - gizmos_origin
+		proj_up_local = proj_up - gizmos_origin
+		proj_back_local = proj_back - gizmos_origin
 
 		# Calculate rotation gizmos
-#		gizmo_rotation_z = Geometry.offset_polygon_2d(_find_curve(proj_right_local, proj_up_local), 0)[0]
 		gizmo_rotation_z = _find_curve(proj_right_local, proj_up_local)
 		gizmo_rotation_y = _find_curve(proj_right_local, proj_back_local)
 		gizmo_rotation_x = _find_curve(proj_up_local, proj_back_local)
@@ -96,15 +94,27 @@ func _draw() -> void:
 			continue
 		if object.selected:
 			draw_multiline(points, selected_color, 1.0, true)
-
 			var width := 1.1
-			draw_set_transform($Gizmos.rect_position, 0, Vector2.ONE)
+			draw_set_transform(gizmos_origin, 0, Vector2.ONE)
 			draw_line(Vector2.ZERO, proj_right_local, Color.red)
 			draw_line(Vector2.ZERO, proj_up_local, Color.green)
 			draw_line(Vector2.ZERO, proj_back_local, Color.blue)
+			_draw_arrow(proj_right_local, Color.red)
+			_draw_arrow(proj_up_local, Color.green)
+			_draw_arrow(proj_back_local, Color.blue)
 			draw_polyline(gizmo_rotation_x, Color.red, width)
 			draw_polyline(gizmo_rotation_y, Color.green, width)
 			draw_polyline(gizmo_rotation_z, Color.blue, width)
+			draw_circle((proj_right_local.normalized() * 8).limit_length(proj_right_local.length()), 1, Color.red)
+			draw_circle((proj_up_local.normalized() * 8).limit_length(proj_up_local.length()), 1, Color.green)
+			draw_circle((proj_back_local.normalized() * 8).limit_length(proj_back_local.length()), 1, Color.blue)
+			var font: Font = Global.control.theme.default_font
+			var font_height := font.get_height()
+			var char_scale := 0.16
+			draw_set_transform(gizmos_origin + Vector2(-font_height / 4, font_height / 4) * char_scale, 0, Vector2.ONE * char_scale)
+			draw_char(font, (proj_right_local.normalized() * 8).limit_length(proj_right_local.length())/char_scale, 'X', '')
+			draw_char(font, (proj_up_local.normalized() * 8).limit_length(proj_up_local.length())/char_scale, "Y", '')
+			draw_char(font, (proj_back_local.normalized() * 8).limit_length(proj_back_local.length())/char_scale, "Z", '')
 			draw_set_transform_matrix(Transform2D())
 		elif object.hovered:
 			draw_multiline(points, hovered_color, 1.0, true)
@@ -125,7 +135,7 @@ func _on_gizmo_button_up() -> void:
 func _find_curve(a: Vector2, b: Vector2) -> PoolVector2Array:
 	var curve2d := Curve2D.new()
 	curve2d.bake_interval = 0.5
-#	var control := (b.linear_interpolate(a, 0.5) - gizmos.rect_position).limit_length(8)
+#	var control := (b.linear_interpolate(a, 0.5) - gizmos_origin).limit_length(8)
 	var control := (b.linear_interpolate(a, 0.5))
 #	print(control.length())
 	a = (a.normalized() * 8).limit_length(a.length())
@@ -135,6 +145,13 @@ func _find_curve(a: Vector2, b: Vector2) -> PoolVector2Array:
 	curve2d.add_point(a, Vector2.ZERO, control)
 	curve2d.add_point(b, control)
 	return curve2d.get_baked_points()
+
+
+func _draw_arrow(a: Vector2, color) -> void:
+	var b := (a + Vector2(-0.5, 1).rotated(a.angle() + PI / 2) * 2)
+	var c := (a + Vector2(0.5, 1).rotated(a.angle() + PI / 2) * 2)
+#	draw_polyline([b, a, c, b], Color.black)
+	draw_primitive([a, b, c], [color, color, color], [])
 
 
 func _scale_gizmo(gizmo: Control, new_scale: Vector2) -> void:
