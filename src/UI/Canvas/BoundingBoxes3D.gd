6614bb3adc364bb8b261f571bcdddf7a5af176b5
Emmanouil Papadeas <manoschool@yahoo.gr>
2022-12-31 03:20:08 +0200

    Use curves for rotation gizmos

--- src/UI/Canvas/BoundingBoxes3D.gd
+++ src/UI/Canvas/BoundingBoxes3D.gd
@@ -4,6 +4,14 @@ var points_per_object := {}
 var selected_color := Color.white
 var hovered_color := Color.gray
 
+#var gizmo_used := false
+var proj_right_local: Vector2
+var proj_up_local: Vector2
+var proj_back_local: Vector2
+var gizmo_rotation_x := PoolVector2Array()
+var gizmo_rotation_y := PoolVector2Array()
+var gizmo_rotation_z := PoolVector2Array()
+
 onready var gizmos: Control = $Gizmos
 
 
@@ -11,6 +19,31 @@ func _ready() -> void:
 	for gizmo in gizmos.get_children():
 		gizmo.connect("button_up", self, "_on_gizmo_button_up")
 		gizmo.connect("button_down", self, "_on_gizmo_button_down", [gizmo.get_index() + 1])
+
+
+func _input(event: InputEvent) -> void:
+	if not event is InputEventMouseButton:
+		return
+	var pos: Vector2 = Global.canvas.current_pixel - gizmos.rect_position
+#	print(pos, " ", gizmo_rotation_z)
+	if event.button_index == BUTTON_LEFT:
+		if event.pressed:
+			if Geometry.is_point_in_polygon(pos, gizmo_rotation_x):
+				for object in points_per_object:
+					if object.selected:
+						object.applying_gizmos = Object3D.Gizmos.X_ROT
+			elif Geometry.is_point_in_polygon(pos, gizmo_rotation_y):
+				for object in points_per_object:
+					if object.selected:
+						object.applying_gizmos = Object3D.Gizmos.Y_ROT
+			elif Geometry.is_point_in_polygon(pos, gizmo_rotation_z):
+				for object in points_per_object:
+					if object.selected:
+						object.applying_gizmos = Object3D.Gizmos.Z_ROT
+		else:
+			for object in points_per_object:
+				if object.selected:
+					object.applying_gizmos = Object3D.Gizmos.NONE
 
 
 func get_points(camera: Camera, object3d: Object3D) -> void:
@@ -25,6 +58,25 @@ func get_points(camera: Camera, object3d: Object3D) -> void:
 	if object3d.selected:
 		gizmos.visible = true
 		gizmos.rect_position = camera.unproject_position(object3d.translation)
+
+		var right :Vector3= object3d.translation + object3d.transform.basis.x
+		var up :Vector3= object3d.translation + object3d.transform.basis.y
+		var back :Vector3= object3d.translation + object3d.transform.basis.z
+
+		var proj_right :Vector2= object3d.camera.unproject_position(right)
+		var proj_up :Vector2= object3d.camera.unproject_position(up)
+		var proj_back :Vector2= object3d.camera.unproject_position(back)
+
+		proj_right_local = proj_right - gizmos.rect_position
+		proj_up_local = proj_up - gizmos.rect_position
+		proj_back_local = proj_back - gizmos.rect_position
+
+		# Calculate rotation gizmos
+#		gizmo_rotation_z = Geometry.offset_polygon_2d(_find_curve(proj_right_local, proj_up_local), 0)[0]
+		gizmo_rotation_z = _find_curve(proj_right_local, proj_up_local)
+		gizmo_rotation_y = _find_curve(proj_right_local, proj_back_local)
+		gizmo_rotation_x = _find_curve(proj_up_local, proj_back_local)
+
 	update()
 
 
@@ -44,70 +96,15 @@ func _draw() -> void:
 			continue
 		if object.selected:
 			draw_multiline(points, selected_color, 1.0, true)
-#			var pixel_diff: float = (object.camera.unproject_position(Vector3.RIGHT) - object.camera.unproject_position(Vector3.ZERO)).length()
-
-			var right :Vector3= object.translation + object.transform.basis.x
-			var up :Vector3= object.translation + object.transform.basis.y
-			var back :Vector3= object.translation + object.transform.basis.z
-
-			var proj_right :Vector2= object.camera.unproject_position(right)
-			var proj_up :Vector2= object.camera.unproject_position(up)
-			var proj_back :Vector2= object.camera.unproject_position(back)
-
-			var proj_right_diff := proj_right - gizmos.rect_position
-			var proj_up_diff := proj_up - gizmos.rect_position
-			var proj_back_diff := proj_back - gizmos.rect_position
-
-#			$Gizmos/XArrow.rect_scale.x = (proj_right_diff).length() / pixel_diff
-#			$Gizmos/XArrow.rect_scale.y = (proj_right_diff).length() / pixel_diff
-#			$Gizmos/XArrow.rect_rotation = rad2deg((proj_right_diff).angle()) + 90
-#
-#			$Gizmos/YArrow.rect_scale.x = (proj_up_diff).length() / pixel_diff
-#			$Gizmos/YArrow.rect_scale.y = (proj_up_diff).length() / pixel_diff
-#			$Gizmos/YArrow.rect_rotation = rad2deg((proj_up_diff).angle()) + 90
-#
-#			$Gizmos/ZArrow.rect_scale.x = (proj_back_diff).length() / pixel_diff
-#			$Gizmos/ZArrow.rect_scale.y = (proj_back_diff).length() / pixel_diff
-#			$Gizmos/ZArrow.rect_rotation = rad2deg(proj_back_diff.angle()) + 90
-#			var z_transform := Transform2D(object.transform)
-#			z_transform.origin = $Gizmos.rect_position
 
 			var width := 1.1
-			draw_line(gizmos.rect_position, proj_right, Color.red)
-			draw_line(gizmos.rect_position, proj_up, Color.green)
-			draw_line(gizmos.rect_position, proj_back, Color.blue)
-#			var p := PoolVector2Array()
-#			p.append(proj_right - Vector2.ONE * width)
-#			p.append(proj_right)
-#			p.append(proj_right + Vector2.ONE * width)
-#			p.append(proj_up + Vector2.ONE * width)
-#			p.append(proj_up)
-#			p.append(proj_up - Vector2.ONE * width)
-#			p.append(proj_right - Vector2.ONE * width)
-
-#			draw_line(proj_right, proj_up, Color.blue, width)
-#			draw_line(proj_right, proj_back, Color.green, width)
-#			draw_line(proj_back, proj_up, Color.red, width)
-#			draw_polyline(p, Color.orange)
-
-			var curve2d := Curve2D.new()
-			curve2d.bake_interval = 3.5
-			var start := proj_right_diff
-			var end := proj_up_diff
-#			var control := (end.linear_interpolate(start, 0.5) - gizmos.rect_position).limit_length(8)
-			var control := (end.linear_interpolate(start, 0.5))
-			print(control.length())
-			start = start.normalized() * control.length()
-			end = end.normalized() * control.length()
-			control = control.normalized() * sqrt(pow(start.length()/4, 2) * 2)
-			print(start.length(), " ", end.length())
-			curve2d.add_point(start, Vector2.ZERO, control)
-			curve2d.add_point(end, control)
-			var baked := curve2d.get_baked_points()
 			draw_set_transform($Gizmos.rect_position, 0, Vector2.ONE)
-			draw_polyline(baked, Color.blue, 1.1)
-			draw_line(Vector2.ZERO, start, Color.orange)
-			draw_line(Vector2.ZERO, control, Color.yellow, 1.0)
+			draw_line(Vector2.ZERO, proj_right_local, Color.red)
+			draw_line(Vector2.ZERO, proj_up_local, Color.green)
+			draw_line(Vector2.ZERO, proj_back_local, Color.blue)
+			draw_polyline(gizmo_rotation_x, Color.red, width)
+			draw_polyline(gizmo_rotation_y, Color.green, width)
+			draw_polyline(gizmo_rotation_z, Color.blue, width)
 			draw_set_transform_matrix(Transform2D())
 		elif object.hovered:
 			draw_multiline(points, hovered_color, 1.0, true)
@@ -123,6 +120,21 @@ func _on_gizmo_button_up() -> void:
 	for object in points_per_object:
 		if object.selected:
 			object.applying_gizmos = Object3D.Gizmos.NONE
+
+
+func _find_curve(a: Vector2, b: Vector2) -> PoolVector2Array:
+	var curve2d := Curve2D.new()
+	curve2d.bake_interval = 0.5
+#	var control := (b.linear_interpolate(a, 0.5) - gizmos.rect_position).limit_length(8)
+	var control := (b.linear_interpolate(a, 0.5))
+#	print(control.length())
+	a = (a.normalized() * 8).limit_length(a.length())
+	b = (b.normalized() * 8).limit_length(b.length())
+	control = control.normalized() * sqrt(pow(a.length()/4, 2) * 2)
+#	print(a.length(), " ", b.length())
+	curve2d.add_point(a, Vector2.ZERO, control)
+	curve2d.add_point(b, control)
+	return curve2d.get_baked_points()
 
 
 func _scale_gizmo(gizmo: Control, new_scale: Vector2) -> void:
